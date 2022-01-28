cffb74ea8093a298316ec877601111127541a530
orcunTasdemir <otasdemir@vassar.edu>
2022-01-28 14:02:10 -0500

    files here for jokes

---
+++ jokes.js
@@ -0,0 +1,93 @@
+var test_stimuli =
+
+    [{
+            "Joke": "I let my accountant do my taxes because it saves time: last spring it saved me ten % %.",
+            "Joke ending": "years",
+            "Nonjoke ending": "days"
+        },
+        {
+            "Joke": "A replacement player hit a home run with my % %.",
+            "Joke ending": "girl",
+            "Nonjoke ending": "ball"
+        },
+        {
+            "Joke": "I still miss my ex-wife, but I am improving my % %.",
+            "Joke ending": "aim",
+            "Nonjoke ending": "ego"
+        },
+        {
+            "Joke": "It is not hard to meet expenses: they are % %.",
+            "Joke ending": "everywhere",
+            "Nonjoke ending": "affordable"
+        },
+        {
+            "Joke": "The last time a guy in a mask took all my money, I was in % %.",
+            "Joke ending": "surgery",
+            "Nonjoke ending": "shock"
+        },
+        {
+            "Joke": "My mechanic could not fix my brakes, so he fixed my % %.",
+            "Joke ending": "horn",
+            "Nonjoke ending": "clutch"
+        },
+        {
+            "Joke": "A man who has lost ninety percent of his brain is called a % %.",
+            "Joke ending": "widower",
+            "Nonjoke ending": "zombie"
+        },
+        {
+            "Joke": "The only ones who want me for my body are % %.",
+            "Joke ending": "mosquitoes",
+            "Nonjoke ending": "losers"
+        },
+        {
+            "Joke": "By the time Mary had her fourteenth child, she'd run out of names to call her % %.",
+            "Joke ending": "husband",
+            "Nonjoke ending": "offspring"
+        },
+        {
+            "Joke": "A device for finding furniture in the dark is a % %.",
+            "Joke ending": "shin",
+            "Nonjoke ending": "candle"
+        },
+        {
+            "Joke": "My doctor has never violated his oath: the oath he took years ago to become a % %.",
+            "Joke ending": "millionaire",
+            "Nonjoke ending": "practitioner"
+        },
+        {
+            "Joke": "The only place where you can find people who spend all their time minding their own business is the % %.",
+            "Joke ending": "cemetery",
+            "Nonjoke ending": "bank"
+        },
+        {
+            "Joke": "It is hard to raise a family nowadays, especially in the % %.",
+            "Joke ending": "morning",
+            "Nonjoke ending": "country"
+        },
+        {
+            "Joke": "I asked the hostess if she remembered me from the party last year, and she said that she never forgets a % %.",
+            "Joke ending": "dress",
+            "Nonjoke ending": "name"
+        },
+        {
+            "Joke": "Everyone had so much fun jumping into the swimming pool, we decided to put in a little % %.",
+            "Joke ending": "water",
+            "Nonjoke ending": "platform"
+        },
+        {
+            "Joke": "A thoughtful wife has pork chops ready when her husband comes home from % %.",
+            "Joke ending": "hunting",
+            "Nonjoke ending": "work"
+        },
+        {
+            "Joke": "Statistics indicate that Americans spend 80 million a year on games of chance, mostly % %.",
+            "Joke ending": "weddings",
+            "Nonjoke ending": "dice"
+        },
+        {
+            "Joke": "She read so much about the bad effects of smoking she decided she'd have to give up % %.",
+            "Joke ending": "reading",
+            "Nonjoke ending": "habit"
+        }
+    ]
