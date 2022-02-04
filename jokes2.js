a33f76e5ae7016488ab4588fd0913b5ac8ce380d
orcunTasdemir <otasdemir@vassar.edu>
2022-02-04 16:59:43 -0500

    updates

--- jokes2.js
+++ jokes2.js
@@ -1,5 +1,5 @@
-var test_stimuli = [
-  [
+var test_stimuli_1 = [
+  
     {
       "Joke stem": "I let my accountant do my taxes because it saves time: last spring it saved me ten",
       "Joke ending": "years",
@@ -134,4 +134,414 @@ var test_stimuli = [
       "NonjokeQuestion": "Nonjokequestion19"
     }
   ]
-]
+
+var test_stimuli_2 = [
+  
+    {
+      "Joke stem": "I let my accountant do my taxes because it saves time: last spring it saved me ten",
+      "Joke ending": "years",
+      "Nonjoke ending": "days",
+      "JokeQuestion": "jokequestion1",
+      "NonjokeQuestion": "Nonjokequestion1"
+    },
+    {
+      "Joke stem": "A replacement player hit a home run with my",
+      "Joke ending": "girl",
+      "Nonjoke ending": "ball",
+      "JokeQuestion": "jokequestion2",
+      "NonjokeQuestion": "Nonjokequestion2"
+    },
+    {
+      "Joke stem": "I still miss my ex-wife, but I am improving my",
+      "Joke ending": "aim",
+      "Nonjoke ending": "ego",
+      "JokeQuestion": "jokequestion3",
+      "NonjokeQuestion": "Nonjokequestion3"
+    },
+    {
+      "Joke stem": "It is not hard to meet expenses: they are",
+      "Joke ending": "everywhere",
+      "Nonjoke ending": "affordable",
+      "JokeQuestion": "jokequestion4",
+      "NonjokeQuestion": "Nonjokequestion4"
+    },
+    {
+      "Joke stem": "The last time a guy in a mask took all my money, I was in",
+      "Joke ending": "surgery",
+      "Nonjoke ending": "shock",
+      "JokeQuestion": "jokequestion5",
+      "NonjokeQuestion": "Nonjokequestion5"
+    },
+    {
+      "Joke stem": "My mechanic could not fix my brakes, so he fixed my",
+      "Joke ending": "horn",
+      "Nonjoke ending": "clutch",
+      "JokeQuestion": "jokequestion6",
+      "NonjokeQuestion": "Nonjokequestion6"
+    },
+    {
+      "Joke stem": "A man who has lost ninety percent of his brain is called a",
+      "Joke ending": "widower",
+      "Nonjoke ending": "zombie",
+      "JokeQuestion": "jokequestion7",
+      "NonjokeQuestion": "Nonjokequestion7"
+    },
+    {
+      "Joke stem": "The only ones who want me for my body are",
+      "Joke ending": "mosquitoes",
+      "Nonjoke ending": "losers",
+      "JokeQuestion": "jokequestion8",
+      "NonjokeQuestion": "Nonjokequestion8"
+    },
+    {
+      "Joke stem": "By the time Mary had her fourteenth child, she’d run out of names to call her",
+      "Joke ending": "husband",
+      "Nonjoke ending": "offspring",
+      "JokeQuestion": "jokequestion9",
+      "NonjokeQuestion": "Nonjokequestion9"
+    },
+    {
+      "Joke stem": "A device for finding furniture in the dark is a",
+      "Joke ending": "shin",
+      "Nonjoke ending": "candle",
+      "JokeQuestion": "jokequestion10",
+      "NonjokeQuestion": "Nonjokequestion10"
+    },
+    {
+      "Joke stem": "My doctor has never violated his oath: the oath he took years ago to become a",
+      "Joke ending": "millionaire",
+      "Nonjoke ending": "practitioner",
+      "JokeQuestion": "jokequestion11",
+      "NonjokeQuestion": "Nonjokequestion11"
+    },
+    {
+      "Joke stem": "The only place where you can find people who spend all their time minding their own business is the",
+      "Joke ending": "cemetery",
+      "Nonjoke ending": "bank",
+      "JokeQuestion": "jokequestion12",
+      "NonjokeQuestion": "Nonjokequestion12"
+    },
+    {
+      "Joke stem": "It is hard to raise a family nowadays, especially in the",
+      "Joke ending": "morning",
+      "Nonjoke ending": "country",
+      "JokeQuestion": "jokequestion13",
+      "NonjokeQuestion": "Nonjokequestion13"
+    },
+    {
+      "Joke stem": "I asked the hostess if she remembered me from the party last year, and she said that she never forgets a",
+      "Joke ending": "dress",
+      "Nonjoke ending": "name",
+      "JokeQuestion": "jokequestion14",
+      "NonjokeQuestion": "Nonjokequestion14"
+    },
+    {
+      "Joke stem": "Everyone had so much fun jumping into the swimming pool, we decided to put in a little",
+      "Joke ending": "water",
+      "Nonjoke ending": "platform",
+      "JokeQuestion": "jokequestion15",
+      "NonjokeQuestion": "Nonjokequestion15"
+    },
+    {
+      "Joke stem": "A thoughtful wife has pork chops ready when her husband comes home from",
+      "Joke ending": "hunting",
+      "Nonjoke ending": "work",
+      "JokeQuestion": "jokequestion16",
+      "NonjokeQuestion": "Nonjokequestion16"
+    },
+    {
+      "Joke stem": "Statistics indicate that Americans spend 80 million a year on games of chance, mostly",
+      "Joke ending": "weddings",
+      "Nonjoke ending": "dice",
+      "JokeQuestion": "jokequestion17",
+      "NonjokeQuestion": "Nonjokequestion17"
+    },
+    {
+      "Joke stem": "She read so much about the bad effects of smoking she decided she’d have to give up",
+      "Joke ending": "reading",
+      "Nonjoke ending": "habit",
+      "JokeQuestion": "jokequestion18",
+      "NonjokeQuestion": "Nonjokequestion18"
+    },
+    {
+      "Joke stem": "I used to have a handle on life, but then it",
+      "Joke ending": "broke",
+      "Nonjoke ending": "failed (?)",
+      "JokeQuestion": "jokequestion19",
+      "NonjokeQuestion": "Nonjokequestion19"
+    }
+  ]
+
+var test_stimuli_3 = [
+  
+    {
+      "Joke stem": "I let my accountant do my taxes because it saves time: last spring it saved me ten",
+      "Joke ending": "years",
+      "Nonjoke ending": "days",
+      "JokeQuestion": "jokequestion1",
+      "NonjokeQuestion": "Nonjokequestion1"
+    },
+    {
+      "Joke stem": "A replacement player hit a home run with my",
+      "Joke ending": "girl",
+      "Nonjoke ending": "ball",
+      "JokeQuestion": "jokequestion2",
+      "NonjokeQuestion": "Nonjokequestion2"
+    },
+    {
+      "Joke stem": "I still miss my ex-wife, but I am improving my",
+      "Joke ending": "aim",
+      "Nonjoke ending": "ego",
+      "JokeQuestion": "jokequestion3",
+      "NonjokeQuestion": "Nonjokequestion3"
+    },
+    {
+      "Joke stem": "It is not hard to meet expenses: they are",
+      "Joke ending": "everywhere",
+      "Nonjoke ending": "affordable",
+      "JokeQuestion": "jokequestion4",
+      "NonjokeQuestion": "Nonjokequestion4"
+    },
+    {
+      "Joke stem": "The last time a guy in a mask took all my money, I was in",
+      "Joke ending": "surgery",
+      "Nonjoke ending": "shock",
+      "JokeQuestion": "jokequestion5",
+      "NonjokeQuestion": "Nonjokequestion5"
+    },
+    {
+      "Joke stem": "My mechanic could not fix my brakes, so he fixed my",
+      "Joke ending": "horn",
+      "Nonjoke ending": "clutch",
+      "JokeQuestion": "jokequestion6",
+      "NonjokeQuestion": "Nonjokequestion6"
+    },
+    {
+      "Joke stem": "A man who has lost ninety percent of his brain is called a",
+      "Joke ending": "widower",
+      "Nonjoke ending": "zombie",
+      "JokeQuestion": "jokequestion7",
+      "NonjokeQuestion": "Nonjokequestion7"
+    },
+    {
+      "Joke stem": "The only ones who want me for my body are",
+      "Joke ending": "mosquitoes",
+      "Nonjoke ending": "losers",
+      "JokeQuestion": "jokequestion8",
+      "NonjokeQuestion": "Nonjokequestion8"
+    },
+    {
+      "Joke stem": "By the time Mary had her fourteenth child, she’d run out of names to call her",
+      "Joke ending": "husband",
+      "Nonjoke ending": "offspring",
+      "JokeQuestion": "jokequestion9",
+      "NonjokeQuestion": "Nonjokequestion9"
+    },
+    {
+      "Joke stem": "A device for finding furniture in the dark is a",
+      "Joke ending": "shin",
+      "Nonjoke ending": "candle",
+      "JokeQuestion": "jokequestion10",
+      "NonjokeQuestion": "Nonjokequestion10"
+    },
+    {
+      "Joke stem": "My doctor has never violated his oath: the oath he took years ago to become a",
+      "Joke ending": "millionaire",
+      "Nonjoke ending": "practitioner",
+      "JokeQuestion": "jokequestion11",
+      "NonjokeQuestion": "Nonjokequestion11"
+    },
+    {
+      "Joke stem": "The only place where you can find people who spend all their time minding their own business is the",
+      "Joke ending": "cemetery",
+      "Nonjoke ending": "bank",
+      "JokeQuestion": "jokequestion12",
+      "NonjokeQuestion": "Nonjokequestion12"
+    },
+    {
+      "Joke stem": "It is hard to raise a family nowadays, especially in the",
+      "Joke ending": "morning",
+      "Nonjoke ending": "country",
+      "JokeQuestion": "jokequestion13",
+      "NonjokeQuestion": "Nonjokequestion13"
+    },
+    {
+      "Joke stem": "I asked the hostess if she remembered me from the party last year, and she said that she never forgets a",
+      "Joke ending": "dress",
+      "Nonjoke ending": "name",
+      "JokeQuestion": "jokequestion14",
+      "NonjokeQuestion": "Nonjokequestion14"
+    },
+    {
+      "Joke stem": "Everyone had so much fun jumping into the swimming pool, we decided to put in a little",
+      "Joke ending": "water",
+      "Nonjoke ending": "platform",
+      "JokeQuestion": "jokequestion15",
+      "NonjokeQuestion": "Nonjokequestion15"
+    },
+    {
+      "Joke stem": "A thoughtful wife has pork chops ready when her husband comes home from",
+      "Joke ending": "hunting",
+      "Nonjoke ending": "work",
+      "JokeQuestion": "jokequestion16",
+      "NonjokeQuestion": "Nonjokequestion16"
+    },
+    {
+      "Joke stem": "Statistics indicate that Americans spend 80 million a year on games of chance, mostly",
+      "Joke ending": "weddings",
+      "Nonjoke ending": "dice",
+      "JokeQuestion": "jokequestion17",
+      "NonjokeQuestion": "Nonjokequestion17"
+    },
+    {
+      "Joke stem": "She read so much about the bad effects of smoking she decided she’d have to give up",
+      "Joke ending": "reading",
+      "Nonjoke ending": "habit",
+      "JokeQuestion": "jokequestion18",
+      "NonjokeQuestion": "Nonjokequestion18"
+    },
+    {
+      "Joke stem": "I used to have a handle on life, but then it",
+      "Joke ending": "broke",
+      "Nonjoke ending": "failed (?)",
+      "JokeQuestion": "jokequestion19",
+      "NonjokeQuestion": "Nonjokequestion19"
+    }
+  ]
+
+var test_stimuli_4 = [
+  
+    {
+      "Joke stem": "I let my accountant do my taxes because it saves time: last spring it saved me ten",
+      "Joke ending": "years",
+      "Nonjoke ending": "days",
+      "JokeQuestion": "jokequestion1",
+      "NonjokeQuestion": "Nonjokequestion1"
+    },
+    {
+      "Joke stem": "A replacement player hit a home run with my",
+      "Joke ending": "girl",
+      "Nonjoke ending": "ball",
+      "JokeQuestion": "jokequestion2",
+      "NonjokeQuestion": "Nonjokequestion2"
+    },
+    {
+      "Joke stem": "I still miss my ex-wife, but I am improving my",
+      "Joke ending": "aim",
+      "Nonjoke ending": "ego",
+      "JokeQuestion": "jokequestion3",
+      "NonjokeQuestion": "Nonjokequestion3"
+    },
+    {
+      "Joke stem": "It is not hard to meet expenses: they are",
+      "Joke ending": "everywhere",
+      "Nonjoke ending": "affordable",
+      "JokeQuestion": "jokequestion4",
+      "NonjokeQuestion": "Nonjokequestion4"
+    },
+    {
+      "Joke stem": "The last time a guy in a mask took all my money, I was in",
+      "Joke ending": "surgery",
+      "Nonjoke ending": "shock",
+      "JokeQuestion": "jokequestion5",
+      "NonjokeQuestion": "Nonjokequestion5"
+    },
+    {
+      "Joke stem": "My mechanic could not fix my brakes, so he fixed my",
+      "Joke ending": "horn",
+      "Nonjoke ending": "clutch",
+      "JokeQuestion": "jokequestion6",
+      "NonjokeQuestion": "Nonjokequestion6"
+    },
+    {
+      "Joke stem": "A man who has lost ninety percent of his brain is called a",
+      "Joke ending": "widower",
+      "Nonjoke ending": "zombie",
+      "JokeQuestion": "jokequestion7",
+      "NonjokeQuestion": "Nonjokequestion7"
+    },
+    {
+      "Joke stem": "The only ones who want me for my body are",
+      "Joke ending": "mosquitoes",
+      "Nonjoke ending": "losers",
+      "JokeQuestion": "jokequestion8",
+      "NonjokeQuestion": "Nonjokequestion8"
+    },
+    {
+      "Joke stem": "By the time Mary had her fourteenth child, she’d run out of names to call her",
+      "Joke ending": "husband",
+      "Nonjoke ending": "offspring",
+      "JokeQuestion": "jokequestion9",
+      "NonjokeQuestion": "Nonjokequestion9"
+    },
+    {
+      "Joke stem": "A device for finding furniture in the dark is a",
+      "Joke ending": "shin",
+      "Nonjoke ending": "candle",
+      "JokeQuestion": "jokequestion10",
+      "NonjokeQuestion": "Nonjokequestion10"
+    },
+    {
+      "Joke stem": "My doctor has never violated his oath: the oath he took years ago to become a",
+      "Joke ending": "millionaire",
+      "Nonjoke ending": "practitioner",
+      "JokeQuestion": "jokequestion11",
+      "NonjokeQuestion": "Nonjokequestion11"
+    },
+    {
+      "Joke stem": "The only place where you can find people who spend all their time minding their own business is the",
+      "Joke ending": "cemetery",
+      "Nonjoke ending": "bank",
+      "JokeQuestion": "jokequestion12",
+      "NonjokeQuestion": "Nonjokequestion12"
+    },
+    {
+      "Joke stem": "It is hard to raise a family nowadays, especially in the",
+      "Joke ending": "morning",
+      "Nonjoke ending": "country",
+      "JokeQuestion": "jokequestion13",
+      "NonjokeQuestion": "Nonjokequestion13"
+    },
+    {
+      "Joke stem": "I asked the hostess if she remembered me from the party last year, and she said that she never forgets a",
+      "Joke ending": "dress",
+      "Nonjoke ending": "name",
+      "JokeQuestion": "jokequestion14",
+      "NonjokeQuestion": "Nonjokequestion14"
+    },
+    {
+      "Joke stem": "Everyone had so much fun jumping into the swimming pool, we decided to put in a little",
+      "Joke ending": "water",
+      "Nonjoke ending": "platform",
+      "JokeQuestion": "jokequestion15",
+      "NonjokeQuestion": "Nonjokequestion15"
+    },
+    {
+      "Joke stem": "A thoughtful wife has pork chops ready when her husband comes home from",
+      "Joke ending": "hunting",
+      "Nonjoke ending": "work",
+      "JokeQuestion": "jokequestion16",
+      "NonjokeQuestion": "Nonjokequestion16"
+    },
+    {
+      "Joke stem": "Statistics indicate that Americans spend 80 million a year on games of chance, mostly",
+      "Joke ending": "weddings",
+      "Nonjoke ending": "dice",
+      "JokeQuestion": "jokequestion17",
+      "NonjokeQuestion": "Nonjokequestion17"
+    },
+    {
+      "Joke stem": "She read so much about the bad effects of smoking she decided she’d have to give up",
+      "Joke ending": "reading",
+      "Nonjoke ending": "habit",
+      "JokeQuestion": "jokequestion18",
+      "NonjokeQuestion": "Nonjokequestion18"
+    },
+    {
+      "Joke stem": "I used to have a handle on life, but then it",
+      "Joke ending": "broke",
+      "Nonjoke ending": "failed (?)",
+      "JokeQuestion": "jokequestion19",
+      "NonjokeQuestion": "Nonjokequestion19"
+    }
+  ]
