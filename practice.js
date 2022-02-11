374298f900a1f578e4b49d84eede012a7d7927e6
LilyYan2001 <lyan@stuy.edu>
2022-02-11 17:05:58 -0500

    practice question js file

---
+++ practice.js
@@ -0,0 +1,72 @@
+ practice_questions = [
+  {
+    Sentence_Stem : "The children ran outside to",
+    Completion: "play",
+    Left_or_Right: "left",
+    Question: "The children are playing.",
+    Correct_Response: "y"
+  }]
+  /*{
+    Sentence_Stem : "On Valentine's Day the woman received a single red",
+    Completion: "rose",
+    Left_or_Right: "right",
+    Question: "The woman got a daisy.",
+    Correct_Response: "n"
+  },
+  {
+    Sentence_Stem : "The princess would someday become a",
+    Completion: "queen",
+    Left_or_Right: "left",
+    Question: "The princess will be a princess forever.",
+    Correct_Response: "n"
+  },
+  {
+    Sentence_Stem : "The Parkers' baby could already say three",
+    Completion: "words",
+    Left_or_Right: "right",
+    Question: "The baby is mute.",
+    Correct_Response: "n"
+  },
+  {
+    Sentence_Stem : "The grandmother left everything to her son in her",
+    Completion: "will",
+    Left_or_Right: "left",
+    Question: "The son received his grandmother's belongings.",
+    Correct_Response: "y"
+  },
+  {
+    Sentence_Stem : "The fluffy white clouds are high up in the",
+    Completion: "sky",
+    Left_or_Right: "right",
+    Question: "The sky is cloudless.",
+    Correct_Response: "n"
+  },
+  {
+    Sentence_Stem : "To promote their album the band went on",
+    Completion: "tour",
+    Left_or_Right: "left",
+    Question: "The band made an album.",
+    Correct_Response: "y"
+  },
+  {
+    Sentence_Stem : "Katie put the flowers in an expensive",
+    Completion: "vase",
+    Left_or_Right: "right",
+    Question: "The vase is pretty pricey.",
+    Correct_Response: "y"
+  },
+  {
+    Sentence_Stem : "The maid dusted the books on the",
+    Completion: "shelf",
+    Left_or_Right: "left",
+    Question: "The maid is slacking off.",
+    Correct_Response: "n"
+  },
+  {
+    Sentence_Stem : "The birthday card was funny and made me",
+    Completion: "laugh",
+    Left_or_Right: "right",
+    Question: "The birthday card made me happy.",
+    Correct_Response: "y"
+  }
+];*/
