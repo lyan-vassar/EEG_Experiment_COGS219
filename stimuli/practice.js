a2b200ef7553968e10669c8827b3e362f3990e2b
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-02-12 15:35:21 -0500

    fix practice stimuli; load experiment in separate js file

--- stimuli/practice.js
+++ stimuli/practice.js
@@ -1,72 +1,102 @@
- practice_questions = [
+[
   {
-    Sentence : "The children ran outside to",
-    Completion: "play",
-    Left_or_Right: "left",
-    Question: "The children are playing.",
-    Correct_Response: "y"
-  }]
-  /*{
-    Sentence_Stem : "On Valentine's Day the woman received a single red",
-    Completion: "rose",
-    Left_or_Right: "right",
-    Question: "The woman got a daisy.",
-    Correct_Response: "n"
+    "id_2": "p1",
+    "sentence": "The children ran outside to",
+    "word": "play",
+    "sentence_type": "practice",
+    "question": "The children did not play.",
+    "correct_response": "n",
+    "left_or_right": "right",
+    "trigger_value": 8
   },
   {
-    Sentence_Stem : "The princess would someday become a",
-    Completion: "queen",
-    Left_or_Right: "left",
-    Question: "The princess will be a princess forever.",
-    Correct_Response: "n"
+    "id_2": "p2",
+    "sentence": "On Valentine’s Day the woman received a single red",
+    "word": "rose",
+    "sentence_type": "practice",
+    "question": "The woman got a lot of gifts on Valentine's Day.",
+    "correct_response": "n",
+    "left_or_right": "left",
+    "trigger_value": 7
   },
   {
-    Sentence_Stem : "The Parkers' baby could already say three",
-    Completion: "words",
-    Left_or_Right: "right",
-    Question: "The baby is mute.",
-    Correct_Response: "n"
+    "id_2": "p3",
+    "sentence": "The princess would someday become a",
+    "word": "queen",
+    "sentence_type": "practice",
+    "question": "The princess would become a queen in the future.",
+    "correct_response": "y",
+    "left_or_right": "left",
+    "trigger_value": 7
   },
   {
-    Sentence_Stem : "The grandmother left everything to her son in her",
-    Completion: "will",
-    Left_or_Right: "left",
-    Question: "The son received his grandmother's belongings.",
-    Correct_Response: "y"
+    "id_2": "p4",
+    "sentence": "The Parkers’ baby could already say three",
+    "word": "words",
+    "sentence_type": "practice",
+    "question": "The Parker's baby can say some words.",
+    "correct_response": "y",
+    "left_or_right": "left",
+    "trigger_value": 7
   },
   {
-    Sentence_Stem : "The fluffy white clouds are high up in the",
-    Completion: "sky",
-    Left_or_Right: "right",
-    Question: "The sky is cloudless.",
-    Correct_Response: "n"
+    "id_2": "p5",
+    "sentence": "The grandmother left everything to her son in her",
+    "word": "will",
+    "sentence_type": "practice",
+    "question": "The grandmother left nothing to her son.",
+    "correct_response": "n",
+    "left_or_right": "right",
+    "trigger_value": 8
   },
   {
-    Sentence_Stem : "To promote their album the band went on",
-    Completion: "tour",
-    Left_or_Right: "left",
-    Question: "The band made an album.",
-    Correct_Response: "y"
+    "id_2": "p6",
+    "sentence": "The fluffy white clouds are high up in the",
+    "word": "sky",
+    "sentence_type": "practice",
+    "question": "There are fluffy white clouds in the sky.",
+    "correct_response": "y",
+    "left_or_right": "left",
+    "trigger_value": 7
   },
   {
-    Sentence_Stem : "Katie put the flowers in an expensive",
-    Completion: "vase",
-    Left_or_Right: "right",
-    Question: "The vase is pretty pricey.",
-    Correct_Response: "y"
+    "id_2": "p7",
+    "sentence": "To promote their album the band went on",
+    "word": "tour",
+    "sentence_type": "practice",
+    "question": "The band promote their album with a tour.",
+    "correct_response": "y",
+    "left_or_right": "right",
+    "trigger_value": 8
   },
   {
-    Sentence_Stem : "The maid dusted the books on the",
-    Completion: "shelf",
-    Left_or_Right: "left",
-    Question: "The maid is slacking off.",
-    Correct_Response: "n"
+    "id_2": "p8",
+    "sentence": "Katie put the flowers in an expensive",
+    "word": "vase",
+    "sentence_type": "practice",
+    "question": "Katie stored the flowers in a cheap vase.",
+    "correct_response": "n",
+    "left_or_right": "right",
+    "trigger_value": 8
   },
   {
-    Sentence_Stem : "The birthday card was funny and made me",
-    Completion: "laugh",
-    Left_or_Right: "right",
-    Question: "The birthday card made me happy.",
-    Correct_Response: "y"
+    "id_2": "p9",
+    "sentence": "The maid dusted the books on the",
+    "word": "shelf",
+    "sentence_type": "practice",
+    "question": "The maid cleaned the books on the shelf.",
+    "correct_response": "y",
+    "left_or_right": "left",
+    "trigger_value": 7
+  },
+  {
+    "id_2": "p10",
+    "sentence": "The birthday card was funny and made me",
+    "word": "laugh",
+    "sentence_type": "practice",
+    "question": "I did not enjoy the birthday card.",
+    "correct_response": "n",
+    "left_or_right": "right",
+    "trigger_value": 8
   }
-];*/
+]
