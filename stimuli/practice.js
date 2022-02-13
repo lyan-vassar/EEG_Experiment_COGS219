558898345edaede84aedda3e8faf3b8086cd4906
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-02-13 11:12:58 -0500

    adjust practice questions

--- stimuli/practice.js
+++ stimuli/practice.js
@@ -4,17 +4,17 @@ var practice_questions = [
     "sentence": "The children ran outside to",
     "word": "play",
     "sentence_type": "practice",
-    "question": "The children did not play.",
+    "question": "The children played inside.",
     "correct_response": "n",
     "left_or_right": "right",
     "trigger_value": 8
   },
   {
     "id_2": "p2",
-    "sentence": "On Valentine's Day the woman received a single red",
+    "sentence": "On Valentine’s Day the woman received a single red",
     "word": "rose",
     "sentence_type": "practice",
-    "question": "The woman got a lot of gifts on Valentine's Day.",
+    "question": "The woman received a bouquet of flowers.",
     "correct_response": "n",
     "left_or_right": "left",
     "trigger_value": 7
@@ -24,17 +24,17 @@ var practice_questions = [
     "sentence": "The princess would someday become a",
     "word": "queen",
     "sentence_type": "practice",
-    "question": "The princess would become a queen in the future.",
+    "question": "The princess was expected to become the queen.",
     "correct_response": "y",
     "left_or_right": "left",
     "trigger_value": 7
   },
   {
     "id_2": "p4",
-    "sentence": "The Parkers' baby could already say three",
+    "sentence": "The Parkers’ baby could already say three",
     "word": "words",
     "sentence_type": "practice",
-    "question": "The Parker's baby can say some words.",
+    "question": "The baby has started to learn to speak.",
     "correct_response": "y",
     "left_or_right": "left",
     "trigger_value": 7
@@ -44,7 +44,7 @@ var practice_questions = [
     "sentence": "The grandmother left everything to her son in her",
     "word": "will",
     "sentence_type": "practice",
-    "question": "The grandmother left nothing to her son.",
+    "question": "The son received no inheritance.",
     "correct_response": "n",
     "left_or_right": "right",
     "trigger_value": 8
@@ -54,7 +54,7 @@ var practice_questions = [
     "sentence": "The fluffy white clouds are high up in the",
     "word": "sky",
     "sentence_type": "practice",
-    "question": "There are fluffy white clouds in the sky.",
+    "question": "There are clouds in the sky.",
     "correct_response": "y",
     "left_or_right": "left",
     "trigger_value": 7
@@ -64,7 +64,7 @@ var practice_questions = [
     "sentence": "To promote their album the band went on",
     "word": "tour",
     "sentence_type": "practice",
-    "question": "The band promote their album with a tour.",
+    "question": "A tour helps the band sell albums.",
     "correct_response": "y",
     "left_or_right": "right",
     "trigger_value": 8
@@ -74,7 +74,7 @@ var practice_questions = [
     "sentence": "Katie put the flowers in an expensive",
     "word": "vase",
     "sentence_type": "practice",
-    "question": "Katie stored the flowers in a cheap vase.",
+    "question": "Katie used a plastic cup to hold the flowers.",
     "correct_response": "n",
     "left_or_right": "right",
     "trigger_value": 8
@@ -84,7 +84,7 @@ var practice_questions = [
     "sentence": "The maid dusted the books on the",
     "word": "shelf",
     "sentence_type": "practice",
-    "question": "The maid cleaned the books on the shelf.",
+    "question": "The maid cleaned the books.",
     "correct_response": "y",
     "left_or_right": "left",
     "trigger_value": 7
